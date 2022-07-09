182f736f6e449c72ce53caab3d3f816457b5e144
Abdulkareemoj <27707189+Abdulkareemoj@users.noreply.github.com>
2022-07-09 21:13:16 +0100

    add dao

--- backend/api/dao/restaurauntDAOs.js
+++ backend/api/dao/restaurauntDAOs.js
@@ -1,30 +1,32 @@
 let restaurants
 
-export default class RestaurantsDAO{
-    static async injectDB(conn){
-        if (restaurants){
+export default class RestaurantsDAO {
+    static async injectDB(conn) {
+        if (restaurants) {
             return
         }
-    try{
-        restaurants=await conn.db(process.env.RESTREVIEWS_NS).collection("restuarants")
-    }catch (e){
-        console.error(
-        `Unable to establish connection handle in restaurant`
-        )
-    }
+        try {
+            restaurants = await conn.db(process.env.RESTREVIEWS_NS).collection("restuarants")
+        } catch (e) {
+            console.error(
+                `Unable to establish connection handle in restaurant`
+            )
+        }
     }
 
-static async getRestaurants({
-    filters = null,
-    page = 0,
-    restaurantsPerPage = 20,
-}= {}){
-    let query
-    if (filters){
-        if ("name" in filters){
-            query={$text: {$search: filters["name"]}}
-        }else if("cuisine" in filters){
-        query={"cuisine" {$eq: filters["cuisine"]}}
-    }}else if("zipcode" in filters)
-}
+    static async getRestaurants({
+        filters = null,
+        page = 0,
+        restaurantsPerPage = 20,
+    } = {}) {
+        let query
+        if (filters) {
+            if ("name" in filters) {
+                query = { $text: { $search: filters["name"] } }
+            } else if ("cuisine" in filters) {
+                query = { "cuisine": { $eq: filters["cuisine"] } }
+            }
+        } else if ("zipcode" in filters)
+            query = { "address.zipcode": { $eq: filters["zipcode"] } }
+    }
 } 
